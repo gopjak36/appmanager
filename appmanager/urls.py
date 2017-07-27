818d3737979ff7cef10cfe2e19b417a6a3f18a31
gopjak36 <gop.jak.36@gmail.com>
2017-07-27 12:14:44 +0300

    create Appointments list: template, url, view

--- appmanager/urls.py
+++ appmanager/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
 
-    
+    # List Appointments url:
+    url(r'^$', 'manager.views.appointments_list', name="appointments_list"),
 
     # Admin Panel url:
     url(r'^admin/', include(admin.site.urls)),
--- manager/views.py
+++ manager/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render
+
+from .models import Appointment
+
+def appointments_list(request):
+    ''' Appointments List method '''
+
+    return render(request, 'manager/appointments_list.html', {'appointments': Appointment.objects.all()})
+
+def appointments_add(request):
+    ''' Appointments Add method '''
+
+    return render(request,'manager/appointments_add.html',{})
